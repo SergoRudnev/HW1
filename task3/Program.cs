5cc8dffa036e29b18d13c8ea73d141bbd39fad63
Sergey Rudnev <sergorudnev@gmail.com>
2022-09-20 22:43:25 +0500

    task3 done

---
+++ task3/Program.cs
@@ -0,0 +1,16 @@
+﻿//выяснить является ли число четным
+using System;
+  class Program
+  {
+    static void Main()
+     {
+       Console.Write("Введите целое число ");
+       int x = int.Parse(Console.ReadLine());
+       double x_2 = x % 2;
+       if (x_2==0) 
+          Console.WriteLine("число четное");
+       else
+       Console.WriteLine("Число нечетное");
+       Console.ReadLine();
+      }
+   }
