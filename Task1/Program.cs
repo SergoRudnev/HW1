7793c7ee0cd11d576054feb51fa8f0dccc0e3cb4
Sergey Rudnev <sergorudnev@gmail.com>
2022-09-20 22:20:43 +0500

    task1 task1 done

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿int CreateNamber()
+{
+    int namber = new Random().Next(10,100);//возвращаем случайное число
+    return namber;
+  
+}
+
+int a = CreateNamber();
+int b = CreateNamber();
+if(a>b)
+{
+   Console.WriteLine($"Число {a} больше числа {b}");
+}
+if(b>a)
+{
+    Console.WriteLine($"Число {b} больше числа {a}");
+}
+
---
+++ task2/Program.cs
@@ -0,0 +1,22 @@
+﻿int CreateNamber()
+{
+    int namber = new Random().Next(10,100);//возвращаем случайное число
+    return namber;
+  
+}
+
+int a = CreateNamber();
+int b = CreateNamber();
+int c = CreateNamber();
+int Max = a;
+if(Max<b)
+{
+   Max = b;
+}
+if(Max<c)
+{
+    Max = c;
+}
+{
+    Console.WriteLine($"Число {Max} наибольшее из трех представленных {a} {b} {c} ");
+}
